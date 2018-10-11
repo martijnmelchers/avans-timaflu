95e8fa3f12606d5b0a1a6628a68628f911e48db3
Jasper van Hienen <jasper.van.hienen@gmail.com>
2018-10-11 16:16:06 +0200

    Start met verkoop

---
+++ verkoop.php
@@ -0,0 +1,25 @@
+<?php
+	/**
+	 * Created by Jasper van Hienen
+	 * For: timaflu
+	 * Date: October 11 2018
+	 *-----------------------------------
+	 * TODO:
+	 *-----------------------------------
+	 * NOTES:
+	 *-----------------------------------
+	 */
+	include ("parts/header.php");
+?>
+<form style="width: 80%; margin: 0 auto;">
+	<div class="form-group" style="display: flex; flex-direction: row; ">
+		<label for="klantnaam">Klantnaam:</label>
+		<input type="text" class="form-control" id="klantnaam" placeholder="Klantnaam">
+		
+		<label for="klantnaam">Klantnummer:</label>
+		<input type="text" class="form-control" id="klantnaam" placeholder="Klantnummer">
+		
+		<label for="klantnaam">Adres:</label>
+		<input type="text" class="form-control" id="klantnaam" placeholder="Adres">
+	</div>
+</form>
