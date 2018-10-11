b212d87f9feb8b0da41acc6c6b9a644c928db76e
MartijnMelchers <martijn@melchers.nu>
2018-10-11 15:53:47 +0200

    alexa dit is episch

---
+++ facturatie.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+<?php include "./parts/header.php"; ?>
+
+<div class="container">
+    <h1>Facturable facturen</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Factuur Nummer</th>
+            <th scope="col">Status</th>
+            <th scope="col">Acties</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>2</td>
+            <td>Verstuurd</td>
+            <td><button class="btn btn-info">Factuur versturen</button></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Verstuurd</td>
+            <td><button class="btn btn-info">Factuur versturen</button></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
