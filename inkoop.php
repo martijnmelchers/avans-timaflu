305207d37993c63aef5615025285443510aa6b78
Sascha Mendel <=>
2018-10-11 15:25:03 +0200

    Voorbeeld

--- inkoop.php
+++ inkoop.php
@@ -1,42 +1,76 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-</head>
-<body>
-    <?php include "./parts/header.php"; ?>
+<?php include "./parts/header.php"; ?>
 
-    <table class="table">
+
+<div class="container">
+
+
+<h1>Inkoop</h1>
+<br>
+<h3>Medicijnen onder minimumvoorraad: </h2>
+<br>
+<table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">bestelCode</th>
+      <th scope="col">Productnaam</th>
+      <th scope="col">Minimumvoorraad</th>
+      <th scope="col">Huidige voorraad</th>
     </tr>
   </thead>
   <tbody>
     <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <th scope="row">77564</th>
+      <td>Xanax 5mg</td>
+      <td>10</td>
+      <td>5</td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
+      <th scope="row">6674</th>
+      <td>Voltaren emugel</td>
+      <td>40</td>
+      <td>30</td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
+      <th scope="row">8347</th>
+      <td>Baap</td>
+      <td>30</td>
+      <td>20</td>
     </tr>
   </tbody>
 </table>
-</body>
-</html>
+
+<hr>
+
+<h3>Bijbestellen</h3>
+
+
+<p>Selecteer een product uit de lijst om bij te bestellen.</p>
+
+
+
+<form action="">
+    <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Selecteer een product.
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">77564 | Xanax 5mg</a>
+        <a class="dropdown-item" href="#">6674 | Voltaren emugel</a>
+        <a class="dropdown-item" href="#">8347 | Baap</a>
+    </div>
+    </div>
+
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    
+    <button class="btn btn-danger">Meld bestelling</button>
+</form>
+
+
+
+</div>
+
+<?php include "./parts/footer.php"; ?> 
