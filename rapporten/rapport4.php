79d17dff2b3f5873df42d4ca919d1d3949f71fda
Jasper van Hienen <jasper.van.hienen@gmail.com>
2018-10-16 15:51:11 +0200

    Rapportfiles toegevoegd

---
+++ rapporten/rapport4.php
@@ -0,0 +1,20 @@
+<?php
+	/**
+	 * Created by Jasper van Hienen
+	 * For: timaflu
+	 * Date: October 16 2018
+	 *-----------------------------------
+	 * TODO:
+	 *-----------------------------------
+	 * NOTES:
+	 *-----------------------------------
+	 */
+	require "../parts/footer.php";
+?>
+
+<div class="container text-center">
+	<h1>Inkooptotalen leveranciers</h1>
+	<img src="">
+</div>
+
+<?php require "../parts/footer.php"; ?>
