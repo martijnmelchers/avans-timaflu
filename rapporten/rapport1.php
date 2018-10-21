034dbb3f2d47e7f78bfe0bc3d2d26aae4c1db205
Jasper v.H <jasper.van.hienen@gmail.com>
2018-10-21 23:39:52 +0200

    foto verplaats voor een html tabel

--- rapporten/rapport1.php
+++ rapporten/rapport1.php
@@ -13,7 +13,7 @@
 ?>
 
 <div class="container text-center">
-	<h1>Overzicht klanten en kortinspercentages</h1>
+	<h1>Overzicht klanten en kortingspercentages</h1>
 	<img src="https://i.imgur.com/J73WeZT.png">
 </div>
 
--- rapporten/rapport4.php
+++ rapporten/rapport4.php
@@ -14,7 +14,42 @@
 
 <div class="container text-center">
 	<h1>Inkooptotalen leveranciers</h1>
-	<img src="https://i.imgur.com/lK7iIY9.png">
+	<table class="table">
+		<thead>
+		<tr>
+			<th>Leverancier</th>
+			<th>Aantal producten</th>
+			<th>Totaalprijs in euro's</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+			<td>Medicijndomein</td>
+			<td>34</td>
+			<td>2034,-</td>
+		</tr>
+		<tr>
+			<td>Paracetemolpaleis</td>
+			<td>13</td>
+			<td>5630,34</td>
+		</tr>
+		<tr>
+			<td>Hoestdrankmakers</td>
+			<td>53</td>
+			<td>1040,-</td>
+		</tr>
+		<tr>
+			<td>Pil-In-Bill</td>
+			<td>21</td>
+			<td>8945,-</td>
+		</tr>
+		<tr>
+			<td>Ali-Express</td>
+			<td>7</td>
+			<td>570,-</td>
+		</tr>
+		</tbody>
+	</table>
 </div>
 
 <?php require "../parts/footer.php"; ?>
